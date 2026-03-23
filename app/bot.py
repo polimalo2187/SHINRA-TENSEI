cab720944d428c54869627b8766628fabe514957
polimalo2187 <carloramirez834@gmail.com>
2026-03-23 12:51:19 -0400

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -36,7 +36,7 @@ if not BOT_TOKEN:
 # NOMBRE DEL BOT (PARA ENLACES Y MENSAJES)
 # ======================================================
 
-BOT_NAME = "HADES ALPHA"
+BOT_NAME = "SHINRA TENSEI"
 
 # ======================================================
 # /START
